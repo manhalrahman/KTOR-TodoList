e7878c74760392be93d9b9243384922bf1e97d5f
manhalrahman <mrmanhal99@gmail.com>
2022-07-12 13:50:12 +0530

    created the ktor template

---
+++ src/main/kotlin/com/manhalrahman/plugins/Routing.kt
@@ -0,0 +1,39 @@
+package com.manhalrahman.plugins
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun Application.configureRouting() {
+
+    // Starting point for a Ktor app:
+    routing {
+        get("/") {
+            call.respondText("Hello TODOList!")
+        }
+
+        get("/todos") {
+
+        }
+
+        get("/todos/{id}") {
+
+        }
+
+        post("/todos") {
+
+        }
+
+        post("/todos/{id}") {
+
+        }
+
+        delete("/todos/{id}") {
+
+        }
+    }
+    routing {
+    }
+}
