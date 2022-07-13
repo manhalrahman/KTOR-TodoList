8cf1aeada3638212669be500ac4baa1cec744047
manhalrahman <mrmanhal99@gmail.com>
2022-07-13 16:46:15 +0530

    added more functions to the DatabaseManager.kt

--- src/main/kotlin/com/manhalrahman/database/DatabaseManager.kt
+++ src/main/kotlin/com/manhalrahman/database/DatabaseManager.kt
@@ -1,6 +1,13 @@
 package com.manhalrahman.database
 
+import com.manhalrahman.entities.ToDo
+import com.manhalrahman.entities.ToDoDraft
 import org.ktorm.database.Database
+import org.ktorm.dsl.delete
+import org.ktorm.dsl.eq
+import org.ktorm.dsl.insertAndGenerateKey
+import org.ktorm.dsl.update
+import org.ktorm.entity.firstOrNull
 import org.ktorm.entity.sequenceOf
 import org.ktorm.entity.toList
 
@@ -25,5 +32,37 @@ class DatabaseManager {
 
     }
 
+    fun getTodo(id: Int): DBTodoEntity? {
+        return ktormDatabase.sequenceOf(DBTodoTable).firstOrNull{it.id eq id}
+    }
+
+    fun addTodo(draft: ToDoDraft): ToDo {
+        val insertedId = ktormDatabase.insertAndGenerateKey(DBTodoTable) {
+            set(DBTodoTable.title, draft.title)
+            set(DBTodoTable.done, draft.done)
+        } as Int
+
+        return ToDo(insertedId, draft.title, draft.done)
+    }
+
+    fun updateTodo(id: Int, draft: ToDoDraft) : Boolean {
+        val updatedRows = ktormDatabase.update(DBTodoTable) {
+            set(DBTodoTable.title, draft.title)
+            set(DBTodoTable.done, draft.done)
+            where {
+                it.id eq id
+            }
+        }
+
+        return updatedRows > 0
+    }
+
+    fun removeTodo(id: Int): Boolean {
+        val deletedRows = ktormDatabase.delete(DBTodoTable) {
+            it.id eq id
+        }
+
+        return deletedRows > 0
+    }
 
 }
